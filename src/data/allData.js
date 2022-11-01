c97f4c7e805777796cf1e1fbce050312f517df2d
davidsimps <davidsimpande@gmail.com>
2022-11-01 13:06:02 +0000

    Update allData.js
    remove irrelevant comments

--- src/data/allData.js
+++ src/data/allData.js
@@ -1,72 +1,7 @@
 import { GET_WORKSTATIONS_URL, GET_ENGINES_URL } from '../constants/API.js' ;
  
- let workStations = [];
- 
-// let workStations = JSON.stringify([
-//     {
-//       name: "Assembly A",
-//       cycleTimeHrs: 1.5,
-//       currentProduct: {
-//         id: "604676cf40ef9e9137f831e7",
-//         entryTime: now - (31 * 60 * 1000)
-//       }
-//     },
-//     {
-//       name: "Assembly B",
-//       cycleTimeHrs: 1,
-//       currentProduct: {
-//         id: "604676cf40ef9e9137f831e7",
-//         entryTime: now - (20 * 60 * 1000)
-//       }
-//     },
-//     {
-//       name: "Assembly C",
-//       cycleTimeHrs: 2,
-//       currentProduct: null
-//     },
-//     {
-//       name: "Inspection",
-//       cycleTimeHrs: 0.5,
-//       currentProduct: {
-//         id: "604676cf40ef9e9137f831e7",
-//         entryTime: now - (5 * 60 * 1000)
-//       }
-//     },
-//   ])
-
+let workStations = [];
 let engines = [];
-// let engines = JSON.stringify([
-//     {
-//       id: "604676cf40ef9e9137f831e7",
-//       serialNumber: "121612129",
-//       model: "5R",
-//       image: "https://i.postimg.cc/MKbJTkrx/5R.jpg"
-//     },
-//     {
-//       id: "60467703c7e7ca6bca3b598c",
-//       serialNumber: "342612171",
-//       model: "2R",
-//       image: "https://i.postimg.cc/RVzz4CdX/2R.jpg"
-//     },
-//     {
-//       id: "6046770b70a8ffd3e95e7dc8",
-//       serialNumber: "9232842345",
-//       model: "8R",
-//       image: "https://i.postimg.cc/cLxytDV3/8R.png"
-//     },
-//     {
-//       id: "604677132b5381c3d19a0dcc",
-//       serialNumber: "784213414",
-//       model: "5R",
-//       image: "https://i.postimg.cc/MKbJTkrx/5R.jpg"
-//     },
-//     {
-//       id: "6046771b40c3fce9a6f8c11f",
-//       serialNumber: "4315112123",
-//       model: "2R",
-//       image: "https://i.postimg.cc/RVzz4CdX/2R.jpg"
-//     },
-//   ])
 
   export async function getWorkStations(){
     await fetch(GET_WORKSTATIONS_URL)
@@ -106,7 +41,7 @@ let engines = [];
     return engines;
   }
 
-  /// return list of engines by engine id
+  /// return list of engines by engine id (ideally this should be an endpoint in thet API
   export function getEngineById(id) {
     
     return engines.find(
